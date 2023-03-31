9febc90461d7900220452390e3018c98704bc722
Honbra <honbra@honbra.com>
2023-03-31 08:27:12 +0200

    Bring back header auth

--- skolaonline-ical-aas/src/endpoint.rs
+++ skolaonline-ical-aas/src/endpoint.rs
@@ -1,17 +1,21 @@
 use chrono::{Duration, Local};
 use rocket::{form::FromForm, http::Status, response::Redirect, Route};
+use rocket_basicauth::BasicAuth;
 use skolaonline_util::basic_auth_decode;
 
 use crate::{calendar_response::CalendarResponse, error::CalendarEndpointError};
 
 pub fn routes() -> Vec<Route> {
-    routes![calendar, calendar_browser, calendar_other]
+    routes![
+        calendar_query_auth,
+        calendar_header_auth,
+        calendar_browser,
+        calendar_other
+    ]
 }
 
 #[derive(Debug, FromForm)]
 struct Args {
-    auth: String,
-
     #[allow(clippy::needless_late_init)]
     #[field(default = 7)]
     days_back: u8,
@@ -20,9 +24,12 @@ struct Args {
     days_forward: u8,
 }
 
-#[get("/?<args..>", format = "text/calendar", rank = 1)]
-async fn calendar(args: Args) -> Result<CalendarResponse, CalendarEndpointError> {
-    let (username, password) = basic_auth_decode(&args.auth)?;
+#[get("/?<auth>&<args..>", format = "text/calendar", rank = 1)]
+async fn calendar_query_auth(
+    auth: &str,
+    args: Args,
+) -> Result<CalendarResponse, CalendarEndpointError> {
+    let (username, password) = basic_auth_decode(auth)?;
 
     let today = Local::now().date_naive();
     let date_from = today - Duration::days(args.days_back.into());
@@ -30,6 +37,22 @@ async fn calendar(args: Args) -> Result<CalendarResponse, CalendarEndpointError>
 
     let calendar =
         skolaonline_ical::fetch_calendar(&username, &password, date_from, Some(date_to)).await?;
+
+    Ok(CalendarResponse(calendar))
+}
+
+#[get("/?<args..>", format = "text/calendar", rank = 2)]
+async fn calendar_header_auth(
+    auth: BasicAuth,
+    args: Args,
+) -> Result<CalendarResponse, CalendarEndpointError> {
+    let today = Local::now().date_naive();
+    let date_from = today - Duration::days(args.days_back.into());
+    let date_to = today + Duration::days(args.days_forward.into());
+
+    let calendar =
+        skolaonline_ical::fetch_calendar(&auth.username, &auth.password, date_from, Some(date_to))
+            .await?;
 
     Ok(CalendarResponse(calendar))
 }
